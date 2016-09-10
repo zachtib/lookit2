1ec46c1ba777e54deac4e30e576f557709d1aa50
= <=>
2016-09-10 18:33:48 -0400

    add values in AboutDialog

--- ui/AboutDialog.py
+++ ui/AboutDialog.py
@@ -5,4 +5,12 @@ from gi.repository import Gtk, Gio
 class AboutDialog(Gtk.AboutDialog):
     def _init(self, *args, **kwargs):
         super()._init(*args, **kwargs)
+
+        self.set_program_name('Lookit')
+        self.set_version('2.0.0')
+        self.set_copyright('(c) 2016 Zach Tibbitts')
+        self.set_authors(['Zach Tibbitts'])
+        self.set_website('https://github.com/zachtib/lookit2')
+        self.set_website_label('Lookit2 on GitHub')
+
         self.show_all()
