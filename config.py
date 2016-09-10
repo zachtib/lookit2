2a3a0f3629801c508984a5f708c3616611da1f3d
= <=>
2016-09-10 18:40:29 -0400

    move config values to separate file

---
+++ config.py
@@ -0,0 +1,7 @@
+program_name = 'Lookit'
+program_version = '2.0.0'
+program_author = 'Zach Tibbitts'
+program_website = 'https://github.com/zachtib/lookit2'
+program_website_label = 'Lookit2 on GitHub'
+
+copyright_string = '(c) 2016 Zach Tibbitts'
--- ui/AboutDialog.py
+++ ui/AboutDialog.py
@@ -2,15 +2,17 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gio
 
+import config
+
 class AboutDialog(Gtk.AboutDialog):
     def _init(self, *args, **kwargs):
         super()._init(*args, **kwargs)
 
-        self.set_program_name('Lookit')
-        self.set_version('2.0.0')
-        self.set_copyright('(c) 2016 Zach Tibbitts')
-        self.set_authors(['Zach Tibbitts'])
-        self.set_website('https://github.com/zachtib/lookit2')
-        self.set_website_label('Lookit2 on GitHub')
+        self.set_program_name(config.program_name)
+        self.set_version(config.program_version)
+        self.set_copyright(config.copyright_string)
+        self.set_authors([config.program_author])
+        self.set_website(config.program_website)
+        self.set_website_label(config.program_website_label)
 
         self.show_all()
